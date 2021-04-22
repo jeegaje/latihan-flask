71d77f455020026d0859b20113834018f9147190
jeegaje <anggajiyanfajar@gmail.com>
2021-04-22 23:26:45 +0700

    tambah render_template method

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        return ('Hello World!')
+        return render_template('index.html')
 
-    return ('Hello World!')
+    return render_template('index.html')
+
+app.run(host='localhost', port=5000, debug=True)
