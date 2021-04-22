bf984839499cd90b3ca4f086106ae0e779545202
jeegaje <anggajiyanfajar@gmail.com>
2021-04-22 23:54:48 +0700

    Tambah konfigurasi ke mysql phpmyadmin

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'jeegaje123'
+app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_UNIX_SOCKET'] = '/opt/lampp/var/mysql/mysql.sock'
+
+mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
