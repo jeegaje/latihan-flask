13ca3f830021dc229996529a40ca32be8418fbe5
jeegaje <anggajiyanfajar@gmail.com>
2021-06-06 20:55:07 +0700

    Tambah form TambahDataMahasiswa dan Login

--- mywebsite/form.py
+++ mywebsite/form.py
@@ -2,9 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+class TambahDataMahasiswa(FlaskForm):
+    nama = StringField('Nama', validators=[DataRequired()])
+    alamat = StringField('Alamat', validators=[DataRequired()])
+    nim = StringField('Nim', validators=[DataRequired()])
+    submit = SubmitField('Tambah')
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
