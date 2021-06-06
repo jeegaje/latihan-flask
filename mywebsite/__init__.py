a31f4db0b381714a049202bcde4044e0256aff8b
jeegaje <anggajiyanfajar@gmail.com>
2021-06-06 20:54:01 +0700

    Tambah flask_bcrypt untuk generate hash password

--- mywebsite/__init__.py
+++ mywebsite/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flaskext.mysql import MySQL
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -17,7 +18,9 @@ mysql.init_app(app)
 
 app.config['SECRET_KEY'] = '66ec6299e1ce91198534fd318937cb61'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:jeegaje123@localhost:3306/flask?unix_socket=/opt/lampp/var/mysql/mysql.sock'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from mywebsite import routes
